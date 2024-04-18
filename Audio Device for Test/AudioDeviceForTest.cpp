897d411be4404b03e4f9c1169dece17b9f4e98c8
MikyMikeMusic <156812735+MikyMikeMusic@users.noreply.github.com>
2024-04-18 12:01:05 +0200

    Adding Audio Device for Test

---
+++ Audio Device for Test/AudioDeviceForTest.cpp
@@ -0,0 +1,23 @@
+#include "AudioDeviceForTest.h"
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AudioDeviceForTest.h"
+
+AudioDeviceForTest::AudioDeviceForTest(int InSampleRate, int InChannels, int InFrames, AudioCallback InCallback, void* InCookie)
+	: SampleRate(InSampleRate), Channels(InChannels), Frames(InFrames), Callback(InCallback), Cookie(InCookie)
+{
+}
+
+AudioDeviceForTest::~AudioDeviceForTest()
+{
+}
+
+void AudioDeviceForTest::MyAudioCallback(float* InBuffer, int NumFrames, int NumChannels, void* Cookie)
+{
+
+	for (int i = 0; i < NumChannels * NumFrames; i++)
+	{
+		InBuffer[i] = 0.0f;
+	}
+}
---
+++ Audio Device for Test/AudioDeviceForTest.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+class AudioDeviceForTest
+{
+public:
+
+	typedef void(*AudioCallback)(float*, int, int, void*);
+
+	AudioDeviceForTest(int SampleRate, int Channels, int Frames, AudioCallback, void* Cookie);
+	~AudioDeviceForTest();
+
+private:
+	int SampleRate;
+	int Channels;
+	int Frames;
+	AudioCallback Callback;
+	void* Cookie;
+
+
+protected:
+	void MyAudioCallback(float* InBuffer, int NumFrames, int NumChannels, void* Cookie);
+};
